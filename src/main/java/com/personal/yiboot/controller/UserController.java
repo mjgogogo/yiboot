17b4c712c04cfc95a9c02bd2dd93287b0c0c2532
mjgogogo <chenmengjiao126@126.com>
2021-07-14 15:06:47 +0800

    集成log4j

--- src/main/java/com/personal/yiboot/controller/UserController.java
+++ src/main/java/com/personal/yiboot/controller/UserController.java
@@ -4,8 +4,10 @@ import com.personal.yiboot.bean.param.UserParam;
 import com.personal.yiboot.bean.pojo.User;
 import com.personal.yiboot.common.page.PageResult;
 import com.personal.yiboot.service.UserService;
+import com.personal.yiboot.utils.LoggerUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    private final static Logger logger = Logger.getLogger(UserController.class);//给类初始化日志对象
     @GetMapping("/userList")
     @ApiOperation(value="用户列表",notes = "分页查询")
     public PageResult<User> findUserList(UserParam param){
-        System.out.println("enter userList");
+        LoggerUtil.info(logger,"pageSize()={0},pageIndex={1}",param.getPageSize(),param.getPageIndex());
         PageResult<User> result = userService.findUserList(param);
         return result;
     }
