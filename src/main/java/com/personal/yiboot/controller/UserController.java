9eab7cb2d6464a166b04ac9e09f67e6ac5276979
mjgogogo <chenmengjiao126@126.com>
2021-07-14 14:25:16 +0800

    集成swagger2

--- src/main/java/com/personal/yiboot/controller/UserController.java
+++ src/main/java/com/personal/yiboot/controller/UserController.java
@@ -4,20 +4,24 @@ import com.personal.yiboot.bean.param.UserParam;
 import com.personal.yiboot.bean.pojo.User;
 import com.personal.yiboot.common.page.PageResult;
 import com.personal.yiboot.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Api("用户相关")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/userList")
-    public PageResult<User> findUserList(){
+    @GetMapping("/userList")
+    @ApiOperation(value="用户列表",notes = "分页查询")
+    public PageResult<User> findUserList(UserParam param){
         System.out.println("enter userList");
-        UserParam param = new UserParam();
         PageResult<User> result = userService.findUserList(param);
         return result;
     }
--- src/main/java/com/personal/yiboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/personal/yiboot/service/impl/UserServiceImpl.java
@@ -20,6 +20,6 @@ public class UserServiceImpl implements UserService {
         //分页
         //PageHelper.startPage(1,10);// 参数里面有分页信息，这里可以去掉
         Page<User> page = (Page)userMapper.findUserList(param);
-        return new PageResult(page.getResult(),page.getTotal());
+        return new PageResult(page.getResult(),page.getTotal(),page.getPageNum(),page.getPageSize(), page.getPages());
     }
 }
