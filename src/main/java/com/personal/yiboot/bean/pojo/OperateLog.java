1d879d3ad420e8915ad63465cf0049951cd78cd8
mjgogogo <chenmengjiao126@126.com>
2021-07-16 11:39:42 +0800

    日志bean

---
+++ src/main/java/com/personal/yiboot/bean/pojo/OperateLog.java
@@ -0,0 +1,125 @@
+package com.personal.yiboot.bean.pojo;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "yb_operate_log")
+public class OperateLog {
+    @Id
+    private Long id;
+
+    @Column(name = "operate_type")
+    private String operateType;
+
+    private String detail;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    @Column(name = "create_user")
+    private String createUser;
+
+    @Column(name = "update_user")
+    private String updateUser;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return operate_type
+     */
+    public String getOperateType() {
+        return operateType;
+    }
+
+    /**
+     * @param operateType
+     */
+    public void setOperateType(String operateType) {
+        this.operateType = operateType;
+    }
+
+    /**
+     * @return detail
+     */
+    public String getDetail() {
+        return detail;
+    }
+
+    /**
+     * @param detail
+     */
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    /**
+     * @return create_time
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * @param createTime
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * @return update_time
+     */
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * @param updateTime
+     */
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    /**
+     * @return create_user
+     */
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    /**
+     * @param createUser
+     */
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    /**
+     * @return update_user
+     */
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    /**
+     * @param updateUser
+     */
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+}
