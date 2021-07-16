455659d857745138298c361db26b2f7074921839
mjgogogo <chenmengjiao126@126.com>
2021-07-16 11:27:20 +0800

    打war包

--- src/main/java/com/personal/yiboot/YibootApplication.java
+++ src/main/java/com/personal/yiboot/YibootApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,10 +13,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.personal.yiboot.dao")
 @EnableSwagger2
 @EnableAsync
-public class YibootApplication {
+public class YibootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YibootApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(YibootApplication.class);
+	}
 }
