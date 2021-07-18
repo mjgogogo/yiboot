739d994602f3e561ab82391a5faedf7952bb44c2
mjgogogo <chenmengjiao126@126.com>
2021-07-18 12:06:44 +0800

    注释打war包的配置

--- src/main/java/com/personal/yiboot/YibootApplication.java
+++ src/main/java/com/personal/yiboot/YibootApplication.java
@@ -4,8 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,14 +11,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.personal.yiboot.dao")
 @EnableSwagger2
 @EnableAsync
-public class YibootApplication extends SpringBootServletInitializer {
+public class YibootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YibootApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(YibootApplication.class);
-	}
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(YibootApplication.class);
+//	}
 }
